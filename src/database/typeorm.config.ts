26649e4e4e940f61b1e44e9dd3c5f43ea683d985
Dawid-Femin <d.femin@wp.pl>
2025-09-08 00:11:34 +0200

    feat: adjusted ssl/tls for database

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -3,14 +3,18 @@ import { ConfigService } from '@nestjs/config';
 
 export const getTypeOrmConfig = async (
     configService: ConfigService,
-): Promise<TypeOrmModuleOptions> => ({
-  type: 'postgres',
-  host: configService.get<string>('DB_HOST'),
-  port: configService.get<number>('DB_PORT'),
-  username: configService.get<string>('DB_USER'),
-  password: configService.get<string>('DB_PASS'),
-  database: configService.get<string>('DB_NAME'),
-  autoLoadEntities: true,
-  synchronize: configService.get<string>('DATABASE_SYNCHRONIZE') === 'true',
-  ssl: false,
-});
+): Promise<TypeOrmModuleOptions> => {
+  const ssl = configService.get<string>('DATABASE_SSL') === 'true';
+
+  return {
+    type: 'postgres',
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USER'),
+    password: configService.get<string>('DB_PASS'),
+    database: configService.get<string>('DB_NAME'),
+    autoLoadEntities: true,
+    synchronize: configService.get<string>('DATABASE_SYNCHRONIZE') === 'true',
+    ssl: ssl ? { rejectUnauthorized: false } : false,
+  };
+};
